9e57d7dfc236aeddd8eca148e60bb08e9dfcb2f8
ksanu <j.plebaniak@gmail.com>
2018-01-27 01:38:59 +0100

    Added listView, that can be changed, and buttons, that can be clicked.

--- app/src/main/java/jakub/remotecontrol/MainActivity.kt
+++ app/src/main/java/jakub/remotecontrol/MainActivity.kt
@@ -7,31 +7,123 @@ import android.os.Bundle
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.IntentFilter
-
-
+import android.view.View
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    var myDevicesList: ArrayList<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val info = findViewById<TextView>(R.id.infoTextView)
+        info.setText("Halo from the other side")
+
+
+        //get listView object from xml
+        val myDevicesListView = findViewById<ListView>(R.id.devicesListView)
+
+        myDevicesList = arrayListOf("device1", "device2")//todo: get all bt devices
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        val adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, myDevicesList)
+
+        if(myDevicesListView != null) {
+            // Assign adapter to ListView
+            myDevicesListView!!.setAdapter(adapter)
+            // ListView Item Click Listener
+            myDevicesListView!!.setOnItemClickListener(object : OnItemClickListener {
+
+                override fun onItemClick(parent: AdapterView<*>, view: View,
+                                         position: Int, id: Long) {
+
+                    // ListView Clicked item index
+
+                    // ListView Clicked item value
+                    val itemValue = myDevicesListView!!.getItemAtPosition(position) as String
+
+                    // Show Alert
+                    Toast.makeText(applicationContext,
+                            "Position :$position  ListItem : $itemValue", Toast.LENGTH_LONG)
+                            .show()
+
+                }
+
+            })
+        }
+
+
+        connectBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                myDevicesList!!.add("hello test2")
+                adapter.notifyDataSetChanged()
+
+
+                Toast.makeText(applicationContext, " connect clicked", Toast.LENGTH_LONG)
+                        .show()
+            }
+        })
+
+
+        findMoreBtn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View) {
+                // Code here executes on main thread after user presses button
+                myDevicesList!!.remove("hello test2")
+                adapter.notifyDataSetChanged()
+                Toast.makeText(applicationContext, " findMore clicked", Toast.LENGTH_LONG)
+                        .show()
+            }
+        })
+/*
         // Register for broadcasts when a device is discovered.
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         registerReceiver(mReceiver, filter)
+
+        //setup bluetooth:
+        setUpBluetooth()
+
+        //display paired devices
+        val values = emptyArray<String>()
+        values.plusElement("test\ttest")
+
+        val pairedDevices = queryForPairedDevices()
+        if (pairedDevices.size > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (device in pairedDevices) {
+                val deviceName = device.getName()
+                val deviceHardwareAddress = device.getAddress() // MAC address
+                values.plusElement(deviceName + "\t" + deviceHardwareAddress)
+            }
+        }
+
+        val listView = findViewById<ListView>(R.id.devicesListView)
+        val adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values)
+        listView.adapter = adapter
+        */
     }
 
     override fun onDestroy() {
         super.onDestroy()
         // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(mReceiver)
+        //unregisterReceiver(mReceiver)
     }
     fun setUpBluetooth() {
         val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (myBluetoothAdapter == null) {
             // Device doesn't support Bluetooth
+            return
         }
         val REQUEST_ENABLE_BT = 123
         if (!myBluetoothAdapter.isEnabled) {
@@ -52,5 +144,14 @@ class MainActivity : AppCompatActivity() {
                 val deviceHardwareAddress = device.address // MAC address
             }
         }
+    }
+
+
+    fun queryForPairedDevices() : Set<BluetoothDevice> {
+
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = mBluetoothAdapter.getBondedDevices()
+        return pairedDevices
+
     }
 }
